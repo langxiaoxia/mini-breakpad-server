936570ce7c627846e0d74b35fab95fd4293f8824
Xiaoxia Lang <xxlang@grandstream.cn>
2023-01-31 11:08:57 +0800

    feat: modify pm2 config.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   "apps": [
     {
-      "name": "breakpad",
+      "name": "mini-pro",
       "cwd": "./",
       "script": "lib/app.js",
       "env": {
         "NODE_ENV": "production",
-        "MINI_BREAKPAD_SERVER_POOL_PATH": "/data/mini-breakpad-server/pro"
+        "MINI_BREAKPAD_SERVER_POOL_PATH": "/data/mini-breakpad-server/pro",
+	"MINI_BREAKPAD_SERVER_PORT": 1127
       },
-      "env_dev": {
+      "log_date_format": "YYYY-MM-DD_HH:mm Z",
+      "merge_logs": true
+    },{
+      "name": "mini-dev",
+      "cwd": "./",
+      "script": "lib/app.js",
+      "env": {
         "NODE_ENV": "development",
         "MINI_BREAKPAD_SERVER_POOL_PATH": "/data/mini-breakpad-server/dev",
         "MINI_BREAKPAD_SERVER_PORT": 8080
